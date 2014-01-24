8afc315868cc3dad84ed6ba370a2032ef88cc8b9
Rulo Kobashikawa <akobashikawa@gmail.com>
2014-01-24 17:54:35 -0500

    Nuevo Server

--- server.js
+++ server.js
@@ -1,6 +1,12 @@
-var http = require('http');
-http.createServer(function(req, res) {
-  res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello World\n');
-}).listen(1337, '127.0.0.1');
-console.log('Servidor corriendo en http://127.0.0.1:1337/');
+var net = require('net');
+var server = net.createServer(function(c) { //'connection' listener
+  console.log('server connected');
+  c.on('end', function() {
+    console.log('server disconnected');
+  });
+  c.write('hello\r\n');
+  c.pipe(c);
+});
+server.listen(8124, function() { //'listening' listener
+  console.log('server bound');
+});
