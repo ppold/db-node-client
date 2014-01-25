a96697fb799e9d87362ea22bb2ac560637940e9d
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2014-01-24 19:37:20 -0500

    Some cleanup

--- reader.js
+++ reader.js
@@ -1,31 +1,24 @@
 var fs = require('fs');
-var tambo = require('./client.js');
+var tambo = require('./tambo.js');
 
 var config = {
   host: "161.132.6.59",
   port: 6666,
-  dbname: "./"
+  dbname: "images"
 }
 
-
-fs.readdir('./', function(err, files) {
-  if (!err) {
-
-    files.forEach(function(file) {
-      fs.readFile(file, function(err, data) {
-
-        if (!err) {
-          tambo.connect(config, function(api) {
+tambo.connect(config, function(api) {
+  fs.readdir('./', function(err, files) {
+    if (!err) {
+      files.forEach(function(file) {
+        fs.readFile(file, function(err, data) {
+          if (!err) {
             api.create(file, data, function(err, data) {
               console.log(data);
             });
-          });
-        };
-        
+          };
+        });
       });
-    });
-
-  }
-
-
+    }
+  });
 });
