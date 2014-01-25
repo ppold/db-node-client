26f5d0931200804e6136c09adc38f5521bacc1cc
Giorgio Leveroni <giorgio.m.leveroni@gmail.com>
2014-01-24 19:27:12 -0500

    Client done :)

--- client.js
+++ client.js
@@ -1,6 +1,6 @@
-var delimiter = '\n';
 var net = require('net');
 
+var delimiter = '\r\n';
 var create_message = function(action, key, value) {
   // don't add delimiter if value is not specified
   value = value || "";
@@ -8,68 +8,43 @@ var create_message = function(action, key, value) {
     value += delimiter;
   }
 
-  return action + delimiter + key + delimiter + value + delimiter + delimiter;
+  return action + delimiter + key + delimiter + value + delimiter + delimiter + delimiter;
 }
 
-var tambo = {
-  connect: function(host, port, callback) {
-    var client = net.connect({host: host, port: port},
-        function() { //'connect' listener
-      console.log('client connected');
-    });
-    client.once('data', function(data) {
-        //console.log(data.toString());
-        //client.end();
-        //
-        console.log("bienvenido", data.toString());
-        callback();
-    });
-    client.on('end', function() {
-      console.log('client disconnected');
-    });
-
-    var send_action = function(action, key, value, callback) {
-      console.log("mandando mensaje");
-      client.once('data', function(data) {
-        //console.log(data.toString());
-        //client.end();
-        //
-        callback(data.toString());
-      });
-      var messageToBeSent = create_message(action, key, value);
-      console.log("el mensaje que se enviará es:\n" + messageToBeSent);
-      client.write(messageToBeSent);
-    }
-
-    return {
-      create: function(key, value, callback) {
-        send_action("CREATE", key, value, callback);
-      },
-      read: function () {
-        send_action("READ", key, callback);
-      },
-      update: function() {
-        send_action("UPDATE", key, value, callback);
-      },
-      remove: function () {
-        send_action("DELETE", key, callback);
-      }
-    };
-  }
-}
-
-var api = tambo.connect("161.132.6.59", 6666, function() {
-  api.create("2nuestra-llave", "nuestro-valor", function(response) {
-    console.log("nuestra respuesta es:" + response);
+var connect = function(config, callback) {
+  var client = net.connect({host: config.host, port: config.port}, function() {
   });
-});
 
-//api.read(key, function(response) {
+  var send_action = function(action, key, value, callback) {
+    client.once('data', function(data) {
+      callback("error", data.toString());
+    });
+    client.write(create_message(action, key, value));
+  }
 
-//});
-//api.update(key, value, function(response) {
+  var api = {
+    create: function(key, value, callback) {
+      send_action("CREATE", key, value, callback);
+    },
+    read: function(key, callback) {
+      send_action("READ", key, null, callback);
+    },
+    update: function(key, value, callback) {
+      send_action("UPDATE", key, value, callback);
+    },
+    remove: function(key, value, callback) {
+      send_action("DELETE", key, null, callback);
+    },
+    close: function() {
+      client.end();
+    }
+  }
 
-//});
-//api.remove(key, function(response) {
+  client.once('data', function(data) {
+    send_action('USE', config.dbname, null, function () {
+      callback(api);
+    });
+  });
+}
 
-//});
+exports.connect = connect;
